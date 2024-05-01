0060151d4d1a378fb95f2bf19252f7ef9d53764c
fidgetingbits <fidgetingbits@users.noreply.github.com>
2024-05-01 21:33:20 +0800

    make gdbinit.py self contained for lazy loading

--- nix/pwndbg.nix
+++ nix/pwndbg.nix
@@ -1,25 +1,25 @@
 {
-  pkgs ? import <nixpkgs> { }
-  , python3 ? pkgs.python3
-  , gdb ? pkgs.gdb
-  , inputs ? null
-}:
-let
+  pkgs ? import <nixpkgs> {},
+  python3 ? pkgs.python3,
+  gdb ? pkgs.gdb,
+  inputs ? null,
+}: let
   binPath = pkgs.lib.makeBinPath ([
-    python3.pkgs.pwntools   # ref: https://github.com/pwndbg/pwndbg/blob/2023.07.17/pwndbg/wrappers/checksec.py#L8
-  ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
-    python3.pkgs.ropper     # ref: https://github.com/pwndbg/pwndbg/blob/2023.07.17/pwndbg/commands/ropper.py#L30
-    python3.pkgs.ropgadget  # ref: https://github.com/pwndbg/pwndbg/blob/2023.07.17/pwndbg/commands/rop.py#L34
-  ]);
+      python3.pkgs.pwntools # ref: https://github.com/pwndbg/pwndbg/blob/2023.07.17/pwndbg/wrappers/checksec.py#L8
+    ]
+    ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+      python3.pkgs.ropper # ref: https://github.com/pwndbg/pwndbg/blob/2023.07.17/pwndbg/commands/ropper.py#L30
+      python3.pkgs.ropgadget # ref: https://github.com/pwndbg/pwndbg/blob/2023.07.17/pwndbg/commands/rop.py#L34
+    ]);
 
   pyEnv = pkgs.poetry2nix.mkPoetryEnv {
-    groups = [];  # put [ "dev" ] to build "dev" dependencies
-    checkGroups = [];  # put [ "dev" ] to build "dev" dependencies
+    groups = []; # put [ "dev" ] to build "dev" dependencies
+    checkGroups = []; # put [ "dev" ] to build "dev" dependencies
     projectDir = inputs.pwndbg;
     python = python3;
     overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
-      pip = python3.pkgs.pip;  # fix infinite loop in nix, look here: https://github.com/nix-community/poetry2nix/issues/1184#issuecomment-1644878841
-      unicorn = python3.pkgs.unicorn;  # fix build for aarch64 (but it will use same version like in nixpkgs)
+      pip = python3.pkgs.pip; # fix infinite loop in nix, look here: https://github.com/nix-community/poetry2nix/issues/1184#issuecomment-1644878841
+      unicorn = python3.pkgs.unicorn; # fix build for aarch64 (but it will use same version like in nixpkgs)
 
       # disable build from source, because rust's hash had to be repaired many times, see: PR https://github.com/pwndbg/pwndbg/pull/2024
       cryptography = super.cryptography.override {
@@ -27,7 +27,7 @@ let
       };
 
       pt = super.pt.overridePythonAttrs (old: {
-        buildInputs = (old.buildInputs or [ ]) ++ [ super.poetry-core ];
+        buildInputs = (old.buildInputs or []) ++ [super.poetry-core];
       });
       capstone = super.capstone.overridePythonAttrs (old: {
         # fix darwin
@@ -43,13 +43,13 @@ let
   };
 
   pwndbgVersion = pkgs.lib.readFile (pkgs.runCommand "pwndbgVersion" {
-    nativeBuildInputs = [ pkgs.python3 ];
-  } ''
-    mkdir pkg
-    cd pkg
-    cp ${inputs.pwndbg + "/pwndbg/lib/version.py"} version.py
-    python3 -c 'import version; print(version.__version__, end="")' > $out
-  '');
+      nativeBuildInputs = [pkgs.python3];
+    } ''
+      mkdir pkg
+      cd pkg
+      cp ${inputs.pwndbg + "/pwndbg/lib/version.py"} version.py
+      python3 -c 'import version; print(version.__version__, end="")' > $out
+    '');
 
   pwndbg = pkgs.stdenv.mkDerivation {
     name = "pwndbg";
@@ -61,19 +61,22 @@ let
       "gdbinit.py"
     ];
 
-    nativeBuildInputs = [ pkgs.makeWrapper ];
+    nativeBuildInputs = [pkgs.makeWrapper];
 
     installPhase = ''
       mkdir -p $out/share/pwndbg
 
       cp -r gdbinit.py pwndbg $out/share/pwndbg
+      # Build self-contained init script for lazy loading from vanilla gdb
+      sed '2 i import sys, os
+      3 i sys.path.append("${pyEnv}/${pyEnv.sitePackages}")
+      4 i os.environ["PATH"] += ":${binPath}"
+      5 i os.environ["LC_CTYPE"] = "C.UTF-8"' -i $out/share/pwndbg/gdbinit.py
 
       ln -s ${pyEnv} $out/share/pwndbg/.venv
 
       makeWrapper ${gdb}/bin/gdb $out/bin/pwndbg \
         --add-flags "--quiet --early-init-eval-command=\"set charset UTF-8\" --early-init-eval-command=\"set auto-load safe-path /\" --command=$out/share/pwndbg/gdbinit.py" \
-        --prefix PATH : ${binPath} \
-        --set LC_CTYPE C.UTF-8
     '';
 
     meta = {
